8014e860d2d09ff3e35de7b188663aff36b43540
jordhy97 <jordhy.fernando>
2018-01-28 11:26:16 +0700

    Added new color constants (silver and cyan), added simple collision detection

--- src/main.c
+++ src/main.c
@@ -27,6 +27,16 @@ typedef struct {
   int y_speed;
 } Gunfire;
 
+typedef struct {
+  int pair;
+  Point points[15][2];
+  int x_speed;
+  int y_speed;
+  int x_borders[2];
+  int y_borders[2];
+  int destroyed;
+} Rocket;
+
 Gunfire gunfire_types[3];
 
 void init_gunfires(int xres, int yres) {
@@ -53,7 +63,7 @@ int main()
 {
   Framebuffer fb;
 
-  FILE *font;
+  FILE *file;
   Alphabet alphabets[26];
   char names[7][50] = {
     "ADITYA PRATAMA",
@@ -86,40 +96,47 @@ int main()
   int gunfires_neff = 0;
   int head = 0;
   int tail = -1;
-  Alphabet rocket;
-  int rocket_offset;
+  Rocket rocket;
 
   fb_init(&fb, "/dev/fb0");
   init_gunfires(fb.vinfo.xres, fb.vinfo.yres);
-  rocket_offset = fb.vinfo.xres;
 
   // Load font
-  font = fopen("../data/font2.txt", "r");
-  if (font) {
-    while (!feof(font)) {
-      fscanf(font, "\n%c ", &letter);
-      fscanf(font, "%d", &alphabets[letter - 'A'].pair);
+  file = fopen("../data/font2.txt", "r");
+  if (file) {
+    while (!feof(file)) {
+      fscanf(file, "\n%c ", &letter);
+      fscanf(file, "%d", &alphabets[letter - 'A'].pair);
       for (i = 0; i < alphabets[letter - 'A'].pair; i++) {
-        fscanf(font, "%d %d %d %d", &x1, &y1, &x2, &y2);
+        fscanf(file, "%d %d %d %d", &x1, &y1, &x2, &y2);
         alphabets[letter - 'A'].points[i][0] = point_create(x1, y1);
         alphabets[letter - 'A'].points[i][1] = point_create(x2, y2);
       }
     }
-    fclose(font);
+    fclose(file);
   }
 
-  // Load rocket
-  font = fopen("../data/rocket.txt", "r");
-  if (font) {
-    while (!feof(font)) {
-      fscanf(font, "%d\n", &rocket.pair);
+  // Load and init rocket
+  file = fopen("../data/rocket.txt", "r");
+  if (file) {
+    while (!feof(file)) {
+      fscanf(file, "%d\n", &rocket.pair);
       for (i = 0; i < rocket.pair; i++) {
-        fscanf(font, "%d %d %d %d\n", &x1, &y1, &x2, &y2);
-        rocket.points[i][0] = point_create(x1, y1);
-        rocket.points[i][1] = point_create(x2, y2);
+        fscanf(file, "%d %d %d %d\n", &x1, &y1, &x2, &y2);
+        rocket.points[i][0] = point_translate(point_scale_up(point_create(x1, y1), ROCKET_SCALE), fb.vinfo.xres, 0);
+        rocket.points[i][1] = point_translate(point_scale_up(point_create(x2, y2), ROCKET_SCALE), fb.vinfo.xres, 0);
       }
+      fscanf(file, "%d %d\n", &rocket.x_borders[0], &rocket.x_borders[1]);
+      fscanf(file, "%d %d\n", &rocket.y_borders[0], &rocket.y_borders[1]);
+      rocket.x_borders[0] = rocket.x_borders[0] * ROCKET_SCALE + fb.vinfo.xres;
+      rocket.x_borders[1] = rocket.x_borders[1] * ROCKET_SCALE + fb.vinfo.xres;
+      rocket.y_borders[0] = rocket.y_borders[0] * ROCKET_SCALE;
+      rocket.y_borders[1] = rocket.y_borders[1] * ROCKET_SCALE;
+      rocket.x_speed = -9;
+      rocket.y_speed = 0;
+      rocket.destroyed = -1;
     }
-    fclose(font);
+    fclose(file);
   }
 
   start = fb.vinfo.yres;
@@ -164,16 +181,34 @@ int main()
      }
     tcflush(0, TCIFLUSH);
 
-    /* Draw rocket */
-    for (i = 0; i < rocket.pair; i++) {
-      fb_draw_line(&fb, point_translate(point_scale_up(rocket.points[i][0], ROCKET_SCALE), rocket_offset, 0),
-      point_translate(point_scale_up(rocket.points[i][1], ROCKET_SCALE), rocket_offset, 0), color_create(0, 255, 255));
+    /* Check collision */
+    i = head;
+    while (rocket.destroyed == -1 && i < head + 3 && i < head + gunfires_neff) {
+      if ((rocket.x_borders[0] <= gunfires[i].start.x && gunfires[i].start.x <= rocket.x_borders[1] &&
+          rocket.y_borders[0] <= gunfires[i].start.y && gunfires[i].start.y <= rocket.y_borders[1]) ||
+          (rocket.x_borders[0] <= gunfires[i].end.x && gunfires[i].end.x <= rocket.x_borders[1] &&
+          rocket.y_borders[0] <= gunfires[i].end.y && gunfires[i].end.y <= rocket.y_borders[1])) {
+            rocket.destroyed = 1;
+            gunfires[i].start = point_translate(gunfires[i].start, gunfires[i].x_speed, -1000);
+            gunfires[i].end = point_translate(gunfires[i].end, gunfires[i].x_speed, -1000);
+          }
+      i++;
     }
-    rocket_offset -= 9;
+
+    /* Draw rocket */
+    if (rocket.destroyed == -1) {
+      for (i = 0; i < rocket.pair; i++) {
+        fb_draw_line(&fb, rocket.points[i][0], rocket.points[i][1], COLOR_CYAN);
+        rocket.points[i][0] = point_translate(rocket.points[i][0], rocket.x_speed, 0);
+        rocket.points[i][1] = point_translate(rocket.points[i][1], rocket.x_speed, 0);
+      }
+      rocket.x_borders[0] += rocket.x_speed;
+      rocket.x_borders[1] += rocket.x_speed;
+    }
 
     /* Draw gunfires */
     for (i = head; i < head + gunfires_neff; i++) {
-      fb_draw_line(&fb, gunfires[i].start, gunfires[i].end, color_create(192, 192, 192));
+      fb_draw_line(&fb, gunfires[i].start, gunfires[i].end, COLOR_SILVER);
       gunfires[i].start = point_translate(gunfires[i].start, gunfires[i].x_speed, gunfires[i].y_speed);
       gunfires[i].end = point_translate(gunfires[i].end, gunfires[i].x_speed, gunfires[i].y_speed);
       if (gunfires[i].start.y < 0) {
@@ -192,6 +227,12 @@ int main()
 
   /* Close the framebuffer */
   fb_close(&fb);
-
+  // for (i = head; i < head + gunfires_neff; i++) {
+  //   printf("%d\n", i);
+  // printf("%d %d\n" ,gunfires[i].start.x, gunfires[i].start.y);
+  //   printf("%d %d\n" ,gunfires[i].end.x, gunfires[i].end.y);
+  // }
+  // printf("%d %d\n", rocket.y_borders[0], rocket.y_borders[1]);
+  // printf("%d %d\n", rocket.x_borders[0], rocket.x_borders[1]);
   return 0;
 }
